ff159bed28bc515559247cfb6b519cb29952d410
31-Cronos-33 <cronos2796@yandex.ru>
2025-02-11 15:40:19 +0300

    Updated settings.py, apps.py, urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('posts/', include('blog.urls')),
+    path('category/', include('blog.urls')),
     path('pages/', include('pages.urls'))
 ]
